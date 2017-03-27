dac1515536daeafc2995c644e3b5b15c5362f164
madgustoo <agha97a@gmail.com>
2017-03-27 01:55:01 -0400

    Details

---
+++ MusicApp/MusicApp/Content/js/Return-to-top.js
@@ -0,0 +1,18 @@
+﻿$(window).scroll(function () {
+    // If page is scrolled more than 50px
+    if ($(this).scrollTop() >= 50) {
+        // Fade in the arrow
+        $('#return-to-top').fadeIn(200);    
+    } else {
+        // Else fade out the arrow
+        $('#return-to-top').fadeOut(200);  
+    }
+});
+
+// When arrow is clicked
+$('#return-to-top').click(function () {
+    // Scroll to top of body
+    $('body,html').animate({
+        scrollTop : 0                      
+    }, 500);
+});
