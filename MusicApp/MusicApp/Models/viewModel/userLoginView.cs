ff4c47c5dc0864260cd21e3ebd9a0cf350888590
Abdel Khouni <algmtl.7@gmail.com>
2017-04-17 16:45:39 -0400

    Login and register DONE + session[username] + FKN sexy form

---
+++ MusicApp/MusicApp/Models/viewModel/userLoginView.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MusicApp.Models.viewModel
+{
+    public class userLoginView
+    {
+        [Required(ErrorMessage = "User name required.")]
+        public string user_name { get; set; }
+        [Required(ErrorMessage = "password required.")]
+        public string mdp_user { get; set; }
+    }
+}
