a6e58330cad122f3f3b440defe535455f0c1c351
Abdel Khouni <algmtl.7@gmail.com>
2017-04-18 01:44:39 -0400

    userAccountControler favorie

--- MusicApp/MusicApp/Controllers/UserAccountController.cs
+++ MusicApp/MusicApp/Controllers/UserAccountController.cs
@@ -16,10 +16,22 @@ namespace MusicApp.Controllers
         public async Task<ActionResult> Index() {
             // select de la bd
             //List<string> favoritedTrackIds = select trackid from favorites where userid = session[userid]
-            List<Track> favoritedTracks = new List<Track>();
-            foreach (string id in favoritedTrackIds) {
+            using (pushmusicwebEntities re = new pushmusicwebEntities())
+            { 
+                var fav = re.favorite.Where(a => a.user_name == Session["username"].ToString()).ToList();
+                List<Track> favoritedTracks = new List<Track>();
+                foreach (favorite itemfav in fav) {
+
+                    foreach (Track itemtrack in favoritedTracks) {
+                        itemtrack.id = itemfav.track_id;
+                        
+                    }
+                }
+
+                foreach (string id in favoritedTrackIds) {
                 Track track = await spotifyService.GetTrack(id);
                 favoritedTracks.Add(track);
+            }
             }
 
             return View(favoritedTracks);
